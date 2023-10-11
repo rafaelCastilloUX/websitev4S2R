a626ed42506ba16cfe0d9a7a2a7e2527fa136a21
rafaelCastilloUX <r.acendra@sell2rent.com>
2023-10-11 17:21:38 -0500

    Invest updated
    add smartphone image
    add animation to invest and why s2r
    fixed why s2r RWD

--- invest.php
+++ invest.php
@@ -3,27 +3,73 @@
     <main>    
         <!--hero-->
         <section class="bg-invest pt-10 d-block position-relative pt-xl-40vh">
-            <div class="container position-relative">
-                <div class="p-5 bg-dark text-center rounded-2 text-white top-xl-25vh w-100"  >
-                    <h2 class="fw-bold fs-1">Get First Access to Single-Family</h2>
-                    <h1 class="fw-bold  display-3"><span class="text-verde">Rental Properties </span> That 
-                        Fit Your <span class="text-verde">Buy Criteria</span> </h1>
-                     <p class="fw-normal fs-5">Work with Sell2Rent to invest in homes across the country through our Sell & Leaseback program.</p>
-                    <div class="container">
-                        <button type="button" class="btn fw-bold btn-verde ">REGISTER NOW <i class="fa-solid fa-chevron-right"></i></button>
-                        <button type="button" class="btn fw-bold btn-verde ">BOOK A MEETING <i class="fa-solid fa-chevron-right"></i></button>
-                    </div><!--/container-->
-                </div>
-            </div>
+            <div class="container">
+                <div class="row p-5 text-center  bg-dark ">
+                    <div class="col-12 col-lg-8 offset-lg-2">
+                        <div class="p-5text-center rounded-2 text-white top-xl-25vh w-100"  >
+                            <h2 class="fw-bold fs-1">Get first access to Single-Family</h2>
+                            <h1 class="fw-bold  display-3"><span class="text-verde">Rental Properties </span> that 
+                                fit your <span class="text-verde">Buy Criteria</span> </h1>
+                             <p class="fw-normal fs-5">Work with Sell2Rent to invest in homes across the country through our Sell & Leaseback program.</p>
+                            <div class="container">
+                                <button type="button" class="btn fw-bold btn-verde btn-lg">REGISTER NOW <i class="fa-solid fa-chevron-right"></i></button>
+                                <button type="button" class="btn fw-bold btn-verde btn-lg">BOOK A MEETING <i class="fa-solid fa-chevron-right"></i></button>
+                            </div><!--/container-->
+                        </div>
+                    </div><!--/col-->
+                </div><!--/row-->
+            </div><!--/container-->
         </section><!--/.Hero-->
         <section class="content position-relative">
             <div class="container">
+                <div class="row" data-aos="fade-in" data-aos-delay="300">
+                    <div class="col">
+                        <div class="hgroup text-center">
+                        <h4 class="fw-bold text-azul mb-0">Exploring my nNvestment options</h4>
+                        <h2 class="fw-semibold display-4">Why Sell2Rent?</h2>
+                        </div><!--Headingroup-->
+                    </div><!--/col-->
+                </div><!--/.row-->
                 <div class="row">
                     <div class="col">
-                        <p>contenido</p>
+                    <div class="dettails">
+                        <h3 class="fw-bold mb-0">Lets grow together</h3>
+                        <p class="mb-0">At Sell2Rent, We Believe In Building Strong & Mutually Beneficial Partnerships. We Are Dedicated To Fostering Collaborative Relationships That Provide Value To Both Parties Involved.</p>
+                        <a href="#" class="btn btn-lg btn-dark fw-bold">REGISTER NOW <i class="fa-solid fa-chevron-right"></i></a><!--CTA-->
+                    </div><!--/detailes -->
+                    </div><!--/col-->
+                    <div class="col text-center">
+                        <img src="img/demo-investWhy.png" alt="Demo" class="img-fluid" width='350'>
                     </div>
-                </div>
-            </div>
+                </div><!--/row-->
+            </div><!--/container-->
+            <div class="container">
+                <div class="row" data-aos="fade-in" data-aos-delay="300">
+                    <div class="col">
+                        <div class="hgroup text-center">
+                        <h4 class="fw-bold text-azul mb-0">The Process</h4>
+                        <h2 class="fw-semibold display-4">How It Works</h2>
+                        </div><!--Headingroup-->
+                    </div><!--/col-->
+                </div><!--/.row-->
+                <div class="row text-center">
+                    <div class="col">
+                        <div class="d-flex flex-column">
+                            <div class="text-azul">STEP</div>
+                            <h5 class="fw-bold">01</h5>
+                            <div class="desc">
+                                <h6 class="fw-bold">Give us your buy box</h6>
+                                <p>2 steps that take Less Than 3 minutes to fill out our form & define your buy-box.</p>
+                            </div><!--/desc-->
+                        </div><!--/flex-->
+
+                    </div><!--/col-->
+                    <div class="col">
+                        <img src="img/smartphone.png" alt="How it works in S2R" class="w-100">
+                    </div><!--/col-->
+                    <div class="col"></div><!--/col-->
+                </div><!--/row -->
+            </div><!--/container-->
         </section>
     </main>
     <?php include('templates/footer.php'); ?>
