94c15ff69b9b69144d475bba7ad3f8a3944a2a05
rafaelCastilloUX <r.acendra@sell2rent.com>
2023-10-12 16:12:23 -0500

    Accordion-template
    Add: 2 new icons , invest page, strategic page
    remove: @font-face for semi-bold
    some CSS issues solved

--- strategic-partners.php
+++ strategic-partners.php
@@ -22,62 +22,6 @@
         <section class="my-5">
             What is a leaseback
         </section>
-        <section class="faq my-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="heading">
-                            <h3 class="fw-bold text-azul">Frequently Asked Questions</h3>
-                            <h2 class="fw-bold">Your Questions Answered</h2>
-                        </div><!--/heading-->
-                    </div><!--/col-->
-                    <div class="col">
-                        <a href="#" class="btn btn-lg btn-dark border-1">SEE ALL FAQ'S <i class="fas fa-chevron-right"></i></a>
-                    </div><!--/col-->
-                </div><!--/row-->
-                <div class="row">
-                    <div class="col">
-                    <div class="accordion" id="accordionFaqs">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Why Choose Sell2Rent?
-                            </button>
-                            </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionFaqs">
-                            <div class="accordion-body">
-                                <p>The majority of our properties are off-market and have never been listed online. Thanks to our sell and Stay program, most of our properties also include a homeowner willing to rent from day one.</p>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Who is the seller?
-                            </button>
-                            </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFaqs">
-                            <div class="accordion-body">
-                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            How long does the buying process Take?
-                            </button>
-                            </h2>
-                            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFaqs">
-                            <div class="accordion-body">
-                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </div><!--/col-->
-                </div><!--/row-->
-            </div><!--/container-->
-        </section><!--/acordion faq-->
+        <?php include('templates/accordionFAQs.php') ?>                                       
     </main>
     <?php include('templates/footer.php'); ?>
