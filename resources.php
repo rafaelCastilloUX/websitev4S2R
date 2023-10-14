846e118d4200e0bb19a2e5d84d4a8d8ad9820ae9
rafaelCastilloUX <r.acendra@sell2rent.com>
2023-10-14 18:43:09 -0500

    Update resources.php

--- resources.php
+++ resources.php
@@ -74,6 +74,65 @@
             </div>
           </div><!--/container-->
         </section><!--/reels-->
+        <section>
+          <div class="container my-5">
+            <div class="row">
+              <div class="col">
+                <form class="d-flex g-3">
+                  <input class="form-control ms-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-verde me-2" type="submit">GO</button>              
+                </form>
+              </div>
+            </div>
+          </div><!--/container-->
+          <div class="container">
+            <div class="row row-cols-1 row-cols-md-2">
+              <div class="col">
+                <div class="card text-white bg-navy mb-3">
+                  <img src="img/s-partners_uniqueproportion.png" class="card-img-top" alt="Photos">
+                  <div class="card-body p-md-5"> 
+                    <div class="d-inline">
+                      <span class="badge text-bg-dark">White Paper</span>
+                      <span class="badge text-bg-dark">June 30, 2023</span>
+                    </div><!--/inline-->
+                      <p class="card-text fs-5">We see a short window of opportunity to invest in European real estate for value.</p>
+                  </div><!--/card-body-->
+                </div><!--/card-->
+              </div><!--/col-->
+              <div class="col">
+                <div class="card text-white bg-navy mb-3">
+                  <img src="img/s-partners_uniqueproportion.png" class="card-img-top" alt="Photos">
+                  <div class="card-body p-md-5"> 
+                    <div class="d-inline">
+                      <span class="badge text-bg-dark">White Paper</span>
+                      <span class="badge text-bg-dark">June 30, 2023</span>
+                    </div><!--/inline-->
+                      <p class="card-text fs-5">We see a short window of opportunity to invest in European real estate for value.</p>
+                  </div><!--/card-body-->
+                </div><!--/card-->
+              </div><!--/col-->
+            </div><!--/row-->
+          </div><!--/container-->
+        </section><!--/-->
+        <section class="mt-3">
+          <div class="container">
+            <div class="row row-cols-1 row-cols-md-2 d-flex justify-content-between align-items-center">
+              <div class="col col-md-8 col-lg-10 text-center text-md-start">
+                <h2 class="fw-bold display-5">Must Reads for you</h2>
+                <h5 class="fw-bold fs-5">SELL2RENT / LEASEBACKS / SELL AND STAY</h5>
+                <p class="fs-5">Explore Our Latest Blogs and Insights Just for You.</p>
+              </div><!--/col-->
+              <div class="col col-md-4 col-lg-2">
+                <a href="#" class="btn btn-verde fw-bold btn-lg w-100">VIEW ALL <i class="fas fa-chevron-right" aria-hidden="true"></i></a>
+              </div><!--/.col-->
+            </div><!--/.row-->
+            <div class="row">
+              <div class="col">
+
+              </div><!--/.col-->
+            </div><!--/.row-->
+          </div><!--/.container-->
+        </section><!--/must read-->
 </main>
 <!--|-->
 <?php include('templates/footer.php'); ?>
