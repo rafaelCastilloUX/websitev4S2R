214b109047188eb587ae1c142d861c91e78f7b42
Frederico <f.rodrigues@sell2rent.com>
2023-10-14 13:21:23 -0300

    Separating JS file

---
+++ resources/batchdata.js
@@ -0,0 +1,44 @@
+var getStartedRedirectURL =  "/forms/index.html";
+
+function isButtonEnabled() {
+    return document.querySelector('#get-started-button').disabled !== false
+}
+
+function setButtonDisabled(value) {
+    document.querySelector('#get-started-button').disabled = value;
+}
+
+function initialize() {
+    setButtonDisabled(true);
+    // Custom Autocomplete of Batch Service
+    batchAutocomplete(document.getElementById("batchAddressAutoComplete"), env.domainServer, env.apiKey);
+
+    // Add an event listener to get value
+    document.addEventListener("batch-address-auto-complete", function (e) {
+      // Here you will get data what you had selected in input by using "e.detail"
+      console.log('Selected item: ', e.detail);
+
+      localStorage.setItem('street', e.detail.address);
+      localStorage.setItem('city', e.detail.city);
+      localStorage.setItem('state', e.detail.state);
+      localStorage.setItem('zipcode', e.detail.zip);
+      setButtonDisabled(false);
+    });
+
+    // Add an event listener to handle error
+    document.addEventListener("batch-auto-complete-error", function (e) {
+      // Here you will get error using "e.detail"
+      console.log("Error while searching: ", e.detail);
+      setButtonDisabled(true);
+    });
+  }
+
+  function goToURL(url) {
+    window.location.href = url;
+  }
+
+  function goToForms() {
+    if(!isButtonEnabled()) {
+        goToURL(getStartedRedirectURL);
+    }
+  }
--- templates/header.php
+++ templates/header.php
@@ -24,38 +24,7 @@
 <link rel="stylesheet" href="css/batchdata.min.css">
 <script src="resources/batchdataAutocomplete/dist/batch-address-auto-complete.min.js"></script>
 <script src="resources/environment.js"></script>
-<script>
-  function initialize() {
-    document.querySelector('#get-started-button').disabled = true;
-    // Custom Autocomplete of Batch Service
-    batchAutocomplete(document.getElementById("batchAddressAutoComplete"), env.domainServer, env.apiKey);
-
-    // Add an event listener to get value
-    document.addEventListener("batch-address-auto-complete", function (e) {
-      // Here you will get data what you had selected in input by using "e.detail"
-      console.log('Selected item: ', e.detail);
-
-      localStorage.setItem('street', e.detail.address);
-      localStorage.setItem('city', e.detail.city);
-      localStorage.setItem('state', e.detail.state);
-      localStorage.setItem('zipcode', e.detail.zip);
-      document.querySelector('#get-started-button').disabled = false;
-    });
-
-    // Add an event listener to handle error
-    document.addEventListener("batch-auto-complete-error", function (e) {
-      // Here you will get error using "e.detail"
-      console.log("Error while searching: ", e.detail);
-      document.querySelector('#get-started-button').disabled = true;
-    });
-  }
-
-  function goToForms() {
-    if(document.querySelector('#get-started-button').disabled === false) {
-      window.location.href = "/forms/index.html"
-    }
-  }
-</script>
+<script src="resources/batchdata.js"></script>
 <!-- End Batchdata -->
 </head>
 
